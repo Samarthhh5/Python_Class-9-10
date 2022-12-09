9295ad6e160a4e2d5e7e0cb5db5bb5e561ea8eed
Samarth Mishra <118279249+Samarthhh5@users.noreply.github.com>
2022-12-09 23:42:05 +0530

    Add files via upload
    A Simple Calculator code

---
+++ Calculator.py
@@ -0,0 +1,43 @@
+
+#ADDING
+
+
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c= a+b
+print(c)
+
+
+
+
+#SUBTRACTION
+
+
+d=int(input("enter the number to subtract from"))
+e=int(input("enter the number to subtract"))
+f=d-e
+print(f)
+
+
+
+
+#DIVISON
+
+
+
+g=int(input("enter the number to divide"))
+h=int(input("enter the number to divide by"))
+i=g/h
+print(i)
+
+
+
+#MULTIPLICATION
+
+
+j=int(input("enter the number to multiply from"))
+k=int(input("enter the number to multiply"))
+l=j*k
+print(l)
+
+
