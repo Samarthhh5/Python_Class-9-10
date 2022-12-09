c1117ff0496c478c3995f7a7fbf78b254c532436
Samarth Mishra <118279249+Samarthhh5@users.noreply.github.com>
2022-12-09 23:10:39 +0530

    Add files via upload
    Simple Interest Calculator
    Simple interest is a basic math which is used to calculate interest for various things mainly when we are taking loans we use it 
    formula-(p*r**t)/100

---
+++ Simple Interest Calculator.py
@@ -0,0 +1,9 @@
+#Simple Interest Calculator
+
+
+p=int(input("Enter Principle amount"))
+rate=int(input("Enter the rate of interest"))
+t=int(input("enter the time"))
+
+
+print("your simple interest is:",(p*rate*t)/100)
