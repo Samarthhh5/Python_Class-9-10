497c839659eb9fda34c7904abb6cbf1c63aadc8a
Samarth Mishra <118279249+Samarthhh5@users.noreply.github.com>
2023-09-06 06:34:36 -0700

    Add files via upload
    Age checker

---
+++ AGE.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age"))
+print("Your age is:", age)
+#Conditional Operators
+
+if(age>18):
+    print("YAYYY!!,You are eligible")
+else:
+    print("Sorry,You aren't Eligible")
